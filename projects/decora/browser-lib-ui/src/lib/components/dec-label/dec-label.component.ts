13884f83bab66bf23f0a172c9f8a20ec61aeb8d1
Jaime Mendes da Silva <jaime.silva@creativedrive.com>
2019-01-16 10:12:18 -0200

    fix(dec-label): DP-2599 - Adicionada borda cinza aos labels que tiverem fundo branco
    Mudança soicitada pelos UX designers

--- projects/decora/browser-lib-ui/src/lib/components/dec-label/dec-label.component.ts
+++ projects/decora/browser-lib-ui/src/lib/components/dec-label/dec-label.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dec-label',
   templateUrl: './dec-label.component.html',
   styleUrls: ['./dec-label.component.scss']
 })
-export class DecLabelComponent {
+export class DecLabelComponent implements OnInit {
   @Input() colorHex?: string;
   @Input() colorClass?: string;
   @Input() stretched?: boolean;
+  borderColor;
+
+  ngOnInit() {
+    if (this.colorHex === '#FFFFFF') {
+      this.borderColor = '#999999';
+    }
+  }
 }
