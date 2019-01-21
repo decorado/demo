40f81029c970f73643ed31554491d40ec506bee0
Bruno Joao <bruno.joao@decoracontent.com>
2019-01-21 10:57:42 -0200

    removed ngu/carousel

--- projects/decora/browser-lib-ui/src/lib/components/dec-zoom-marks-gallery/dec-zoom-marks-gallery.module.ts
+++ projects/decora/browser-lib-ui/src/lib/components/dec-zoom-marks-gallery/dec-zoom-marks-gallery.module.ts
@@ -4,23 +4,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DecZoomMarksGalleryComponent } from './dec-zoom-marks-gallery.component';
 import { DecZoomMarksModule } from './../dec-zoom-marks/dec-zoom-marks.module';
-import { NguCarouselModule } from '@ngu/carousel';
 import { DecImageModule } from './../../directives/image/image.module';
 import { MatIconModule } from '@angular/material';
 import { DecMarkdownsCommentModule } from './../dec-markdowns-comment/dec-markdowns-comment.module';
 import { DecIconModule } from './../dec-icon/dec-icon.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { DecMarkdownsZoomAreaModule } from './../dec-markdowns-zoom-area/dec-markdowns-zoom-area.module';
+import { DecCarouselModule } from './../dec-carousel/dec-carousel.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    NguCarouselModule,
     DecZoomMarksModule,
     DecImageModule,
     MatIconModule,
     DecMarkdownsCommentModule,
     DecIconModule,
+    DecCarouselModule,
     FlexLayoutModule,
     DecMarkdownsZoomAreaModule,
     DecMeshQaModule,
--- projects/decora/browser-lib-ui/src/lib/components/gallery/dec-gallery.component.ts
+++ projects/decora/browser-lib-ui/src/lib/components/gallery/dec-gallery.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CarouselConfig } from './carousel-config';
-import { NguCarouselStore } from '@ngu/carousel/src/ngu-carousel/ngu-carousel.interface';
 
 @Component({
   selector: 'dec-gallery',
@@ -15,35 +13,20 @@ export class DecGalleryComponent {
 
   imgExternalLink: string;
 
-  isFirst: boolean;
-
-  isLast: boolean;
-
-  carouselConfig = CarouselConfig;
-
   index = 0;
 
   @Input()
   set images(value: any[]) {
-
     value = value || new Array<any>();
-
     if (value && (JSON.stringify(value) !== JSON.stringify(this._images))) {
-
       this.imageHighlight = value[0];
-
       this._images = value;
-
       this.setExternalLink();
-
     }
-
   }
 
   get images(): any[] {
-
     return this._images;
-
   }
 
   // ***UPLOAD*** //
@@ -61,57 +44,20 @@ export class DecGalleryComponent {
   constructor() { }
 
   onSelectImage = ($event, sysFile, i) => {
-
     if (this.activeImage && this.activeImage !== $event.target) {
-
       this.activeImage.className = '';
-
     }
-
     $event.target.className = 'active';
-
     this.activeImage = $event.target;
-
     this.imageHighlight = sysFile;
-
     this.index = i;
-
     this.setExternalLink();
-
   }
 
   setExternalLink = () => {
-
     if (this.imageHighlight) {
-
       this.imgExternalLink = this.imageHighlight.fileUrl;
-
     }
-
-  }
-
-  onInitDataFn(event: NguCarouselStore) {
-
-    this.setPrevNextCheckers(event.isFirst, event.items >= this.images.length);
-
-  }
-
-  onMoveFn(event: NguCarouselStore) {
-
-    this.setPrevNextCheckers(event.isFirst, event.isLast);
-
-  }
-
-  setPrevNextCheckers(first: boolean, last: boolean) {
-
-    setTimeout(() => {
-
-      this.isFirst = first;
-
-      this.isLast = last;
-
-    }, 0);
-
   }
 
   uploadedFunction(event) {
